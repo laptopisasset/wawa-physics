7104aa72b48563e2a59d7658a9804b02df271bf8
Soundhar Natarajan <soundrnr@amazon.com>
2023-11-04 09:52:19 +0530

    Learnt basics physics.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import {Canvas} from "@react-three/fiber";
-import './App.css';
+import { Canvas } from "@react-three/fiber";
+import "./App.css";
 import { Experience } from "./components/Experience";
+import { Physics } from "@react-three/rapier";
+import { Suspense } from "react";
 
 function App() {
-
   return (
-    <Canvas camera={{position: [3, 3, 3], fov: 30}}>
-      <color attach="background" args={["#ececec"]}/>
-      <Experience />
+    <Canvas camera={{ position: [10, 10, 10], fov: 30 }}>
+      <color attach="background" args={["#ececec"]} />
+      <Suspense>
+        <Physics debug>
+          <Experience />
+        </Physics>
+      </Suspense>
     </Canvas>
-  )
+  );
 }
 
-export default App
+export default App;
